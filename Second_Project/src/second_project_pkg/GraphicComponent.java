1080308e07c7fb261eab24e40ebed76a6c68525c
Tilden24 <Tilden24@DESKTOP-4SNBBH7.mshome.net>
2021-10-06 10:33:38 -0700

    Update -- added bounce

--- Second_Project/src/second_project_pkg/GraphicComponent.java
+++ Second_Project/src/second_project_pkg/GraphicComponent.java
@@ -10,21 +10,31 @@ import javax.swing.JComponent;
 public class GraphicComponent extends JComponent{
 	
 	//object variables
-	int [] x = {0,50,100,50};
-	int [] y = {0,200,0,10}; 
+	private int [] x = {0,50,100};
+	private int [] y = {0,100,0}; 
+	
+	private int dx, dy;
 	
 	//object constructor
-	public GraphicComponent() {
+	public GraphicComponent(int dx_in, int dy_in) {
+		dx = dx_in;
+		dy = dy_in;
 	}
 
 	//object methods
 	public void paintComponent(Graphics g) {		
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setPaint(Color.RED);
-		g2.fillPolygon(x, y, 4);
+		g2.fillPolygon(x, y, 3);
 	}
 	
-	public void move(int dx, int dy) {
+	public void move() {
+		if (x[0] < 0 || x[2] + 20 > Second_Project_Main.WINDOW_W) {
+			dx*=-1;
+		}
+		if (y[0] < 0 || y[1] + 35 > Second_Project_Main.WINDOW_H) {
+			dy*=-1;
+		}
 		for (int i=0; i<3; i++) {
 			x[i]+= dx;
 			y[i]+= dy;
--- Second_Project/src/second_project_pkg/Second_Project_Main.java
+++ Second_Project/src/second_project_pkg/Second_Project_Main.java
@@ -6,30 +6,30 @@ import javax.swing.JFrame;
 
 
 public class Second_Project_Main {
-
+	final static int WINDOW_W = 600;
+	final static int WINDOW_H = 400;
 	
 	//main method
 	public static void main(String[] args) {
 		// comment
 		JFrame frame = new JFrame();
-		frame.setSize(600,400);
+		frame.setSize(WINDOW_W,WINDOW_H);
 		frame.setTitle("Window");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		GraphicComponent component = new GraphicComponent();
+		GraphicComponent component = new GraphicComponent(1,1);
 		frame.add(component);
 		
 		
 		frame.setVisible(true);
 		
 		while(true) {
-			component.move(1,1);
+			component.move();
 //			component.dumpValues();
 			component.repaint();
 			try {
 				TimeUnit.MILLISECONDS.sleep(10);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
