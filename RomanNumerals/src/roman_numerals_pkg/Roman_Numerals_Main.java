4d831c3e58795fdfcd33ae046cf0ba3c88cfd7da
Tilden24 <Tilden24@DESKTOP-4SNBBH7.mshome.net>
2021-10-22 10:20:47 -0700

    Initial commit

---
+++ RomanNumerals/src/roman_numerals_pkg/Roman_Numerals_Main.java
@@ -0,0 +1,27 @@
+package roman_numerals_pkg;
+
+import java.util.Scanner;
+
+public class Roman_Numerals_Main {
+
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		boolean done = false;
+		while(!done) {
+			System.out.printf("Enter a number to be converted: ");
+			int input = in.nextInt();
+			if (input!=0) {
+				//RomanNumber number = new RomanNumber(input);
+				//number.display();
+			} 
+			else {
+			done=true;
+			}
+		}
+		
+	}
+
+}
