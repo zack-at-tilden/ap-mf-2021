5b1cdbce741600c2f5617457be73b49a5fe8c36a
Tilden24 <Tilden24@DESKTOP-4SNBBH7.mshome.net>
2021-10-22 10:47:42 -0700

    Spacing correction

--- RomanNumerals/src/roman_numerals_pkg/RomanNumber.java
+++ RomanNumerals/src/roman_numerals_pkg/RomanNumber.java
@@ -1,5 +1,32 @@
 package roman_numerals_pkg;
 
+
+import java.util.HashMap;
+
 public class RomanNumber {
+	
+	static private HashMap<String,Integer> numbers = new HashMap<String,Integer>();
+	static {
+		numbers.put("I",1);
+		numbers.put("V",5);
+		numbers.put("X",10);
+		numbers.put("L",50);
+		numbers.put("C",100);
+		numbers.put("D",500);
+		numbers.put("M",100);
+	}
+	
+
+	private int number;
+	
+	public RomanNumber(int number_in) {
+		number = number_in;
+	}
+
+	
+	public int getNumber() {
+		return number;
+	}
+	
 
 }
