888bf47b446499bb684f5f5cdcbdaad4a7a38780
Tilden24 <Tilden24@DESKTOP-4SNBBH7.mshome.net>
2021-11-01 10:36:16 -0700

    Finished basic version.

--- TerminalAverage/src/terminal_average_pkg/Terminal_Average_Main.java
+++ TerminalAverage/src/terminal_average_pkg/Terminal_Average_Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Terminal_Average_Main {
 	
 
-	private static double sum;
+	private static double sum=0;
+	private static int count=0;
 	
 	public Terminal_Average_Main() {
-		//do nothing
+		//place holder
 	}
 
 	public static void main(String[] args) {
@@ -19,10 +20,15 @@ public class Terminal_Average_Main {
 		while(in.hasNextDouble()) {
 			Double value=in.nextDouble();
 			System.out.printf("%f\n",value);
+			count++;
+			sum+=value;
 			System.out.printf("Enter a decimal value (non-numeric to quit): ");
 		}
-
+		System.out.printf("The sum of the values is %f\n",sum);
+		System.out.printf("The average of the values is %f\n",sum/count);
 		System.out.printf("Exiting. Bye!");
+		
+		in.close();
 	}
 
 }
