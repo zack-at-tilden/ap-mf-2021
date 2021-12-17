a13fbc04b4d99346642ce462ba0505f0cdad0ae9
Tilden24 <Tilden24@DESKTOP-4SNBBH7>
2021-12-17 10:46:54 -0800

    Completed the 'seat' part. Working on 'price' part.

--- SeatingPriceChart_Prog/src/SeatPChrting/ThSeatPrcChart_Main.java
+++ SeatingPriceChart_Prog/src/SeatPChrting/ThSeatPrcChart_Main.java
@@ -19,15 +19,34 @@ public class ThSeatPrcChart_Main {
 		Scanner select = new Scanner(System.in);
 		System.out.printf("Seat Choice or Price Choice? (answer 's' for Seat, answer 'p' for Price) >");
 		String seatorprice = select.nextLine();
-		if (seatorprice=="s") {
+		if (seatorprice.equals("s")) {
+			for (int i = 0;i < 9;i++) {
+				for (int j = 0;j < 10;j++) {
+					System.out.printf("%d ",seating[i][j]);
+				}
+				System.out.println();
+			}
 			System.out.printf("Which row? (from 0 to 8 based on farthest to closest) >");
 			int row = select.nextInt();
+			for (int j = 0;j < 10;j++) {
+				System.out.printf("%d ",seating[row][j]);
+			}
+			System.out.println();
 			System.out.printf("Which column? (from 0 to 9 from right side of theater to left side of theater) >");
 			int column = select.nextInt();
 			if (seating[row][column] > 0) {
+				System.out.printf("%d ",seating[row][column]);
 				seating[row][column] = 0;
 				System.out.printf("request reserved");
 			}
+		} else if (seatorprice.equals("p")) {
+			for (int i = 8;i > -1;i--) {
+				for (int j = 0;j < 10;j++) {
+					System.out.printf("%d ",seating[i][j]);
+				}
+				System.out.println();
+			}
+			System.out.printf("[placeholder]");
 		}
 		
 	}
