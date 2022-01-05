48ac75c3e5cb0cb89baba05d3f6613b879f3fed9
Tilden24 <Tilden24@DESKTOP-4SNBBH7>
2022-01-05 10:53:10 -0800

    Initial commit

---
+++ LadybirdBeetleGrid/src/LadyBug.java
@@ -0,0 +1,32 @@
+//possible variables: xpos, ypos, spots, color scaling, size, hibernating?, life stage, threatened?, hungry?
+public class LadyBug {
+
+	int xpos, ypos, spots, life_stage;
+	boolean hibernating, threatened, hungry;
+	double color, size;
+	
+	public LadyBug(int x,int y,int spotcount,double coloring,double sizing) {
+		xpos = x;
+		ypos = y;
+		spots = spotcount;
+		color = coloring;
+		size = sizing;
+		
+		life_stage = 0;
+		hibernating = false;
+		threatened = false;
+		hungry = false;
+	}
+	
+	public void move(int dx,int dy) {
+		xpos += dx;
+		ypos += dy;
+	}
+	
+	public void grow() {
+		if (life_stage < 3) {
+			life_stage++;
+		}
+	}
+	
+}
